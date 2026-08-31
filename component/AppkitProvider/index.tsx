1044f15a91e29440b96ca3b34d3f77cb2dad9eb3
cong_bacoor <cong@bacoor.co>
2026-08-31 15:37:50 +0700

    chore: update AppKit networks from mainnet and arbitrum to base

--- component/AppkitProvider/index.tsx
+++ component/AppkitProvider/index.tsx
@@ -2,7 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createAppKit } from "@reown/appkit/react";
-import { mainnet, arbitrum } from "@reown/appkit/networks";
+import { base } from "@reown/appkit/networks";
 import React, { type ReactNode } from "react";
 import { cookieToInitialState, WagmiProvider, type Config } from "wagmi";
 import { projectId, wagmiAdapter } from "@/config/appkit";
@@ -26,8 +26,8 @@ const metadata = {
 const modal = createAppKit({
   adapters: [wagmiAdapter],
   projectId,
-  networks: [mainnet, arbitrum],
-  defaultNetwork: mainnet,
+  networks: [base],
+  defaultNetwork: base,
   metadata: metadata,
   features: {
     analytics: true, // Optional - defaults to your Cloud configuration
