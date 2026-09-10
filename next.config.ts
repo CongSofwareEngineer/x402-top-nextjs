8f31f4e09fb8a6a29d154d457f2d2c7ebd4e8002
cong_bacoor <cong@bacoor.co>
2026-09-10 10:58:09 +0700

    feat: implement CORS middleware and update payment-signature header configurations

--- next.config.ts
+++ next.config.ts
@@ -1,56 +1,55 @@
-import type { NextConfig } from "next";
-const isProduction = process.env.NODE_ENV === "production";
+import type { NextConfig } from 'next'
+const isProduction = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/api/:path*",
+        source: '/api/:path*',
         headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET, POST, OPTIONS" },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
           {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "Content-Type, Payment-Signature, payment-signature, PAYMENT-SIGNATURE, Payment-Required, payment-required, Authorization",
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Payment-Signature, payment-signature, PAYMENT-SIGNATURE, Payment-Required, payment-required, Authorization',
           },
           // Allow Client JavaScript to read headers returned from Server
           {
-            key: "Access-Control-Expose-Headers",
-            value: "Payment-Required, payment-required, PAYMENT-REQUIRED",
+            key: 'Access-Control-Expose-Headers',
+            value: 'Payment-Required, payment-required, PAYMENT-REQUIRED, Payment-Signature, payment-signature, PAYMENT-SIGNATURE',
           },
         ],
       },
-    ];
+    ]
   },
-};
+}
 
 if (isProduction) {
-  nextConfig.cleanDistDir = true;
+  nextConfig.cleanDistDir = true
   nextConfig.compiler = {
     removeConsole: true,
     styledComponents: {
       displayName: true,
       ssr: true,
     },
-  };
+  }
   nextConfig.experimental = {
     optimizeCss: true,
     gzipSize: true,
     optimizePackageImports: [
-      "@wagmi/core",
-      "@reown/appkit-adapter-wagmi",
-      "@reown/appkit",
-      "viem",
-      "@x402/core",
-      "@x402/evm",
-      "@x402/fetch",
-      "@x402/next",
-      "@x402/svm",
-      "wagmi",
-      "x402",
+      '@wagmi/core',
+      '@reown/appkit-adapter-wagmi',
+      '@reown/appkit',
+      'viem',
+      '@x402/core',
+      '@x402/evm',
+      '@x402/fetch',
+      '@x402/next',
+      '@x402/svm',
+      'wagmi',
+      'x402',
     ],
-  };
+  }
 }
 
-export default nextConfig;
+export default nextConfig
